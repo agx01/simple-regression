ab87ff3e6f193c811890e295e78ca88bb8343790
Arijit Ganguly <arijit.ganguly004@gmail.com>
2018-05-27 14:12:30 +0100

    Completed the course and updated files

--- simple_regression.py
+++ simple_regression.py
@@ -14,4 +14,26 @@ y = dataset.iloc[:,1].values
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
-#Feature Scaling - The library will take care of the feature scaling for this dataset
+#Simple Linear Regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting Test Results
+y_pred = regressor.predict(X_test)
+
+#Visualizing the training set results
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train,regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualizing the test set results
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train,regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
